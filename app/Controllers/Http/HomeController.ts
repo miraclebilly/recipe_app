4275d3d20c367253335393d35a0af9461669dafa
Miracle Billy <uniquemiraculous@gmail.com>
2022-08-01 01:37:33 +0100

    Add Search

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -6,8 +6,9 @@ export default class HomeController{
 
     async index({view, request}: HttpContextContract){
         const page = request.input('page', 1)
+        const query = request.input('query', '')
         const limit = 8
-        const recipes = await Recipe.query().orderBy('created_at', 'desc').preload('user').paginate(page, limit)
+        const recipes = await Recipe.query().whereILike('title', `%${query}%`).orderBy('created_at', 'desc').preload('user').paginate(page, limit)
         // recipes.baseUrl('/')
         return view.render('home', {recipes} )
     }
