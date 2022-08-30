f7cbab35621c656bc32215e100c9db679853b795
Miracle Billy <uniquemiraculous@gmail.com>
2022-08-30 14:18:57 +0100

    Likes Added

--- config/database.ts
+++ config/database.ts
@@ -41,9 +41,9 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
-        ssl: {
-          rejectUnauthorized: Env.get('NODE_ENV') == 'development'
-        }
+        // ssl: {
+        //   rejectUnauthorized: Env.get('NODE_ENV') == 'development'
+        // }
       },
       migrations: {
         naturalSort: true,
